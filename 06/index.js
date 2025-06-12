32900343199b34582ac4613d1bcae6e188f79fbd
Zubizeru <jose.ehmendes@senacsp.edu.br>
2025-06-11 22:12:44 -0300

    feat: atualizando

--- 06/index.js
+++ 06/index.js
@@ -1,98 +1,162 @@
-// Declarando variáveis e importando módulos necessários
+// =======================
+// IMPORTAÇÕES E CONFIGURAÇÕES INICIAIS
+// =======================
 const express = require('express')
 const exphbs = require('express-handlebars')
 const mysql = require('mysql2')
+
 const app = express()
 
-// Configurando o Express para usar o Handlebars como template engine
+// Configura o Handlebars como template engine
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 
-// Middleware para interpretar dados de formulários
+// Middlewares para interpretar dados de formulários e JSON
 app.use(
-    express.urlencoded({
-        extended: true
-    }),
+  express.urlencoded({
+    extended: true,
+  }),
 )
-// Middleware para interpretar JSON
 app.use(express.json())
 
-// Permite servir arquivos estáticos, como CSS,imagens e JavaScript
+// Middleware para servir arquivos estáticos da pasta 'public'
 app.use(express.static('public'))
 
-// Middleware para interpretar JSON
+// =======================
+// ROTAS PRINCIPAIS
+// =======================
+
+// Rota inicial (home)
 app.get('/', function (req, res) {
-    res.render('home')
+  res.render('home')
 })
 
-// Rota para listar os livros
-app.post('/books/insertbook', function (req, res) {
-  const title = req.body.title
-  const pageqty = req.body.pageqty
- 
-  const query = `INSERT INTO books (title, pageqty) VALUES ('${title}', ${pageqty})`
- 
-  conn.query(query, function (err) {
+// =======================
+// CRUD - CREATE
+// =======================
+
+// Rota para inserir um novo filme
+app.post('/movies/insertmovie', function (req, res) {
+  const { title, director, year, genre, duration } = req.body
+
+  const query = "INSERT INTO movies (title, director, year, genre, duration) VALUES (?, ?, ?, ?, ?)"
+
+  conn.query(query, [title, director, year, genre, duration], function (err) {
     if (err) {
       console.log(err)
     }
- 
-    res.redirect('/')
+    res.redirect('/movies')
   })
 })
 
-// Rota para inserir um livro
-app.get('/books', function (req, res) {
-  const query = `SELECT * FROM books`
- 
+// =======================
+// CRUD - READ (LISTAR TODOS)
+// =======================
+
+// Rota para listar todos os filmes
+app.get('/movies', function (req, res) {
+  const query = "SELECT * FROM movies"
+
   conn.query(query, function (err, data) {
     if (err) {
       console.log(err)
     }
- 
-    const books = data
- 
-    console.log(data)
- 
-    res.render('books', { books })
+    res.render('movies', { movies: data })
   })
 })
 
-// Rota para exibir detalhes de um livro específico 
-app.get('/books/:id', function (req, res) {
+// =======================
+// CRUD - READ (DETALHE)
+// =======================
+
+// Rota para exibir detalhes de um filme pelo id
+app.get('/movies/:id', function (req, res) {
   const id = req.params.id
- 
-  const query = `SELECT * FROM books WHERE id = ${id}`
- 
-  conn.query(query, function (err, data) {
+
+  const query = "SELECT * FROM movies WHERE id = ?"
+
+  conn.query(query, [id], function (err, data) {
     if (err) {
       console.log(err)
     }
- 
-    const book = data[0]
- 
-    console.log(data[0])
- 
-    res.render('book', { book })
+    const movie = data[0]
+    res.render('movie', { movie })
   })
 })
 
-// Rota para listar os usuários
-const conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'nodemysql'
-});
+// =======================
+// CRUD - UPDATE (EXIBIR FORMULÁRIO)
+// =======================
 
-// Tratando erro de conexao no MySQL
-conn.connect(function (err) {
+// Rota para exibir o formulário de edição de um filme
+app.get('/movies/edit/:id', function (req, res) {
+  const id = req.params.id
+
+  const query = "SELECT * FROM movies WHERE id = ?"
+
+  conn.query(query, [id], function (err, data) {
     if (err) {
-        console.log(err);
+      console.log(err)
     }
+    const movie = data[0]
+    res.render('editmovie', { movie })
+  })
+})
 
-    console.log('Conectado ao MySQL!');
+// =======================
+// CRUD - UPDATE (SALVAR ALTERAÇÕES)
+// =======================
 
-    app.listen(3000)
-});
+// Rota para atualizar os dados de um filme
+app.post('/movies/updatemovie', function (req, res) {
+  const { id, title, director, year, genre, duration } = req.body
 
+  const query = "UPDATE movies SET title = ?, director = ?, year = ?, genre = ?, duration = ? WHERE id = ?"
+
+  conn.query(query, [title, director, year, genre, duration, id], function (err) {
+    if (err) {
+      console.log(err)
+    }
+    res.redirect(`/movies/edit/${id}`)
+  })
+})
+
+// =======================
+// CRUD - DELETE
+// =======================
+
+// Rota para remover um filme pelo id
+app.post('/movies/remove/:id', function (req, res) {
+  const id = req.params.id
+
+  const query = "DELETE FROM movies WHERE id = ?"
+
+  conn.query(query, [id], function (err) {
+    if (err) {
+      console.log(err)
+    }
+    res.redirect('/movies')
+  })
+})
+
+// =======================
+// CONEXÃO COM O BANCO DE DADOS
+// =======================
+
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'nodemysqlmovies',
+})
+
+// Inicia o servidor apenas após conectar ao banco
+conn.connect(function (err) {
+  if (err) {
+    console.log(err)
+  }
+
+  console.log('Conectado ao MySQL!')
+
+  app.listen(3000)
+})
